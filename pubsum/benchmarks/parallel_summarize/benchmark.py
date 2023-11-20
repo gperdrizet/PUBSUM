f657865c3e3d8915c6c9db42e51a8321f5620693
George Perdrizet <george@perdrizet.org>
2023-11-19 23:43:05 -0500

    Added new benchmark to test data parallel summarization on single GPU

---
+++ pubsum/benchmarks/parallel_summarize/benchmark.py
@@ -0,0 +1,238 @@
+import os
+import pandas as pd
+import psycopg2
+import time
+import multiprocessing as mp
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, GenerationConfig
+
+def benchmark(db_name, user, passwd, host, resume, results_dir, 
+              num_abstracts, device_map_strategies, num_jobs, gpus):
+    
+    print(f'\nRunning data parallel summarization benchmark. Resume = {resume}\n')
+
+    # If we are resuming a prior run, read old data and collect the
+    # completed conditions as a list of lists so we can skip them
+    if resume == 'True':
+
+        # Read existing results if any
+        if os.path.exists(f'{results_dir}/results.csv'):
+
+            old_results_df = pd.read_csv(f'{results_dir}/results.csv')
+
+            completed_runs = list(zip(
+                old_results_df['device_map_strategy'].to_list(),
+                old_results_df['num_jobs'].to_list()
+            ))
+
+            print(f'Resuming benchmark with {len(completed_runs)} runs complete.')
+
+        else:
+            print(f'No data to resume from, starting from scratch.')
+            completed_runs = []
+
+
+    # If we are not resuming an old run, empty datafile if it exists
+    else:
+        # Initialize and save empty results object
+        results = Results(results_dir)
+        results.save_result(overwrite = True)
+
+        # Set completed runs to empty list
+        completed_runs = []
+
+    # Build parameter sets, excluding any which are already complete
+    parameter_sets = []
+
+    for device_map_strategy in device_map_strategies:
+        for jobs in num_jobs:
+            parameter_set = (device_map_strategy, jobs)
+
+            if parameter_set not in completed_runs:
+                parameter_sets.append(parameter_set)
+
+    for parameter_set in parameter_sets:
+
+        run_device_map_strategy = parameter_set[0]
+        run_jobs = parameter_set[1]
+        run_abstracts = num_abstracts // run_jobs
+        print(f'\nStarting benchmark with {run_jobs} concurrent jobs and {run_abstracts} abstracts per job.')
+
+        # Make results object for run
+        results = Results(results_dir)
+        results.data['device_map_strategy'].append(run_device_map_strategy)
+        results.data['num_jobs'].append(run_jobs)
+
+        # Instantiate pool
+        pool = mp.Pool(
+            processes = run_jobs,
+            maxtasksperchild = 1
+        )
+
+        # Start timer
+        start = time.time()
+
+        # Loop on jobs for this run
+
+        for i in list(range(0, run_jobs)):
+
+            result = pool.apply_async(start_job,
+                args = (i, db_name, user, passwd, host, run_abstracts, run_device_map_strategy),
+                callback = collect_result
+            )
+
+        # Clean up
+        pool.close()
+        pool.join()
+
+        # Stop the timer and log the result
+        dT = time.time() - start
+        results.data['summarization_time'].append(dT)
+        results.save_result()
+
+
+def start_job(i, db_name, user, passwd, host, num_abstracts, device_map_strategy):
+    
+    print(f'Job {i} starting.')
+    # Fire up the model for this run
+    model, tokenizer, gen_cfg = start_llm(device_map_strategy)
+
+    # Get an abstract to summarize
+    rows = get_rows(db_name, user, passwd, host, num_abstracts)
+
+    row_count = 1
+
+    for row in rows:
+
+        # Get abstract text for this row
+        abstract = row[1]
+
+        # Make sure this abstract actually has content to be summarized
+        if abstract != None:
+
+            # Do the summary
+            summary = summarize(abstract, model, tokenizer, gen_cfg, device_map_strategy)
+            print(f'Job {i}: finished abstract {row_count}.')
+
+        else:
+            print(f'Job {i}: abstract {row_count} empty.')
+
+        row_count += 1
+
+
+    print(f'Job {1}: done.')
+
+    return True
+
+def get_rows(db_name, user, passwd, host, num_abstracts):
+        
+    # Open connection to PUBMED database on postgreSQL server, create connection
+    connection = psycopg2.connect(f'dbname={db_name} user={user} password={passwd} host={host}')
+
+    # Start new reader cursor
+    read_cursor = connection.cursor()
+
+    # Get the rows
+    read_cursor.execute('SELECT * FROM abstracts ORDER BY random() LIMIT %s', (num_abstracts,))
+
+    return read_cursor
+
+
+def start_llm(device_map_strategy):
+        
+        # Translate device map strategy for huggingface. Start with
+        # device map set to CPU only by default
+        device_map = 'cpu'
+
+        if device_map_strategy == 'multi-GPU':
+            device_map = 'auto'
+
+        elif device_map_strategy == 'single GPU':
+            device_map = 'cuda:0'
+
+        elif device_map_strategy == 'balanced':
+            device_map = 'balanced'
+
+        elif device_map_strategy == 'balanced_low_0':
+            device_map = 'balanced_low_0'
+
+        elif device_map_strategy == 'sequential':
+            device_map = 'sequential'
+
+        model = AutoModelForSeq2SeqLM.from_pretrained("haining/scientific_abstract_simplification", device_map = device_map)
+        
+        # Load generation config from model and set some parameters as desired
+        gen_cfg = GenerationConfig.from_model_config(model.config)
+        gen_cfg.max_length = 256
+        gen_cfg.top_p = 0.9
+        gen_cfg.do_sample = True
+
+        # Initialize the tokenizer
+        tokenizer = AutoTokenizer.from_pretrained("haining/scientific_abstract_simplification")
+
+        # Set prompt to prepend to abstracts
+        #instruction = "summarize, simplify, and contextualize: "
+
+        return model, tokenizer, gen_cfg
+
+def summarize(abstract, model, tokenizer, gen_cfg, device_map_strategy):
+        
+    # Prepend the prompt to this abstract and encode
+    encoding = tokenizer(
+        'summarize, simplify, and contextualize: ' + abstract, 
+        max_length = 672, 
+        padding = 'max_length', 
+        truncation = True, 
+        return_tensors = 'pt'
+    )
+
+    # Move to GPU if appropriate
+    if device_map_strategy != 'CPU only':
+        encoding = encoding.to('cuda')
+    
+    # Generate summary
+    decoded_ids = model.generate(
+        input_ids = encoding['input_ids'],
+        attention_mask = encoding['attention_mask'], 
+        generation_config = gen_cfg
+    )
+    
+    # Decode summary
+    summary = tokenizer.decode(decoded_ids[0], skip_special_tokens = True)
+
+    return summary
+
+def collect_result(result):
+    return True
+
+class Results:
+    '''Class to hold objects and methods for
+    collection of results'''
+
+    def __init__(self, results_dir):
+
+        # Output file for results
+        self.output_file = f'{results_dir}/results.csv'
+
+        # Independent vars for run
+        self.data = {}
+        self.data['num_jobs'] = []
+        self.data['device_map_strategy'] = []
+        self.data['summarization_time'] = []
+
+    def save_result(self, overwrite = False):
+
+        # Make dataframe of new results
+        results_df = pd.DataFrame(self.data)
+
+        if overwrite == False:
+
+            # Read existing results if any and concatenate new results
+            if os.path.exists(self.output_file):
+                old_results_df = pd.read_csv(self.output_file)
+                results_df = pd.concat([old_results_df, results_df])
+
+        else:
+            print('Clearing any old results.')
+
+        # Save results for run to csv
+        results_df.to_csv(self.output_file, index = False)
--- pubsum/run_benchmarks.py
+++ pubsum/run_benchmarks.py
@@ -3,6 +3,7 @@ import config as conf
 import benchmarks.load_summarize_insert.benchmark as lsi
 import benchmarks.sql_insert.benchmark as sql
 import benchmarks.huggingface_device_map.benchmark as device_map
+import benchmarks.parallel_summarize.benchmark as parallel
 
 ###########################
 # Benchmarking parameters #
@@ -13,19 +14,26 @@ benchmark_dir = f'{conf.PROJECT_ROOT_PATH}/benchmarks/'
 
 # Initial MVP load, summarize, insert execution time benchmark
 summarize_benchmark_results_dir = f'{benchmark_dir}/load_summarize_insert'
-summarize_benchmark_abstracts = 3
-summarize_benchmark_replicates = 3
+summarize_benchmark_abstracts = 10
+summarize_benchmark_replicates = 10
 
 # PostgreSQL/psycopg2 insert benchmark for table creation
 insert_benchmark_results_dir = f'{benchmark_dir}/sql_insert'
 insert_benchmark_abstracts = [1000, 20000, 40000]
 insert_strategies = ['execute_many', 'execute_batch', 'execute_values', 'mogrify', 'stringIO']
-insert_benchmark_replicates = 3
+insert_benchmark_replicates = 10
 
 # Huggingface device map benchmark for abstract summarization
 device_map_benchmark_results_dir = f'{benchmark_dir}/huggingface_device_map'
-device_map_benchmark_abstracts = 3
+device_map_benchmark_abstracts = 10
 device_map_strategies = ['CPU only', 'multi-GPU', 'single GPU', 'balanced', 'balanced_low_0', 'sequential']
+
+# Data parallel summarization benchmark
+parallel_summarize_benchmark_results_dir = f'{benchmark_dir}/parallel_summarize'
+parallel_summarize_benchmark_abstracts = 12
+parallel_summarize_device_map_strategies = ['single GPU']
+parallel_summarize_num_jobs = [1, 2, 3] # More than 3 jobs on a single GK210 crashes OOM.
+parallel_summarize_gpus = ['cuda:0, cuda:1, cuda:2, cuda:3']
 
 if __name__ == "__main__":
 
@@ -60,6 +68,13 @@ if __name__ == "__main__":
         choices=[str(True), str(False)],
         default=str(False),
         help='Run huggingface device map benchmark?'
+    )
+
+    parser.add_argument(
+        '--parallel_summarize',
+        choices=[str(True), str(False)],
+        default=str(False),
+        help='Run data parallel summarization benchmark?'
     )
 
     parser.add_argument(
@@ -118,4 +133,20 @@ if __name__ == "__main__":
             device_map_benchmark_results_dir,
             device_map_benchmark_abstracts,
             device_map_strategies
+        )
+
+    # Data parallel summarization benchmark
+    if args.parallel_summarize == 'True':
+
+        parallel.benchmark(
+            conf.DB_NAME,
+            conf.USER,
+            conf.PASSWD, 
+            conf.HOST,
+            args.resume,
+            parallel_summarize_benchmark_results_dir,
+            parallel_summarize_benchmark_abstracts,
+            parallel_summarize_device_map_strategies,
+            parallel_summarize_num_jobs,
+            parallel_summarize_gpus
         )
