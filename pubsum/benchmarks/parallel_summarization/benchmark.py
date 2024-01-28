71e2023211667b5e537e1c6620d2c21e4811ec90
George Perdrizet <george@perdrizet.org>
2024-01-28 11:35:27 -0500

    Moved system memory tracking to worker process.

--- pubsum/benchmarks/parallel_summarization/benchmark.py
+++ pubsum/benchmarks/parallel_summarization/benchmark.py
@@ -147,8 +147,8 @@ def benchmark(
                             # If this is a CPU run, start tracemalloc to monitor
                             # system memory use. Getting this from torch does
                             # not give a sane value.
-                            if 'CPU' in device:
-                                tracemalloc.start()
+                            # if 'CPU' in device:
+                            #     tracemalloc.start()
 
                             # Instantiate pool with one member for each job we need to run
                             pool = mp.Pool(
@@ -204,25 +204,13 @@ def benchmark(
                             # Get the results
                             result = [async_result.get() for async_result in async_results]
 
-                            # Get total model memory footprint
+                            # Collect max memory and model footprint from workers
+                            total_max_memory = 0
                             total_model_footprint = 0
 
                             for worker_result in result:
+                                total_max_memory += worker_result[1]
                                 total_model_footprint += worker_result[2]
-
-                            # Get max memory footprint across workers
-                            total_max_memory = 0
-
-                            # For GPU jobs
-                            if 'GPU' in device:
-                                for worker_result in result:
-                                    total_max_memory += worker_result[1]
-
-                            # And for CPU jobs
-                            elif 'CPU' in device:
-                                memory, max_memory = tracemalloc.get_traced_memory()
-                                total_max_memory = max_memory * 1024
-                                tracemalloc.stop()
 
                             print(f'\n Worker returns: {result}')
                             print(f' Total max memory: {round(total_max_memory / (1024**3), 2)}')
@@ -278,7 +266,7 @@ def start_job(
         else:
             use_GPU = False
             torch.set_num_threads(threads_per_CPU_worker)
-            #tracemalloc.start()
+            tracemalloc.start()
             print(f' Job {i} starting on CPU with {threads_per_CPU_worker} threads per worker')
         
         # Fire up the model for this run
@@ -322,10 +310,9 @@ def start_job(
         max_memory = torch.cuda.max_memory_allocated()
 
     else:
-    #     memory, max_memory = tracemalloc.get_traced_memory()
-    #     max_memory = max_memory * 1024
-    #     tracemalloc.stop()
-        max_memory = None
+        memory, max_memory = tracemalloc.get_traced_memory()
+        max_memory = max_memory * 1024
+        tracemalloc.stop()
 
     model_memory = model.get_memory_footprint()
 
